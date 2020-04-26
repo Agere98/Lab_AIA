5fbd1b96d21ed541ce9a27fbb48fe1776d22dda5
Agere98 <44338036+Agere98@users.noreply.github.com>
2020-04-26 23:43:18 +0200

    Add list sorting

--- React/react-app/src/components/Collection.js
+++ React/react-app/src/components/Collection.js
@@ -9,6 +9,7 @@ class Collection extends React.Component {
     this.state = {
       items: [],
       filter: "",
+      orderBy: "name asc",
     };
     this.addItem = this.addItem.bind(this);
     this.removeItem = this.removeItem.bind(this);
@@ -48,6 +49,20 @@ class Collection extends React.Component {
       .filter((item) =>
         item.name.toLowerCase().includes(this.state.filter.toLowerCase())
       )
+      .sort((a, b) => {
+        const aName = a.name.toLowerCase();
+        const bName = b.name.toLowerCase();
+        switch (this.state.orderBy) {
+          case "name desc":
+            return aName > bName ? -1 : aName < bName ? 1 : 0;
+          case "rating asc":
+            return a.rating - b.rating;
+          case "rating desc":
+            return b.rating - a.rating;
+          default:
+            return aName > bName ? 1 : aName < bName ? -1 : 0;
+        }
+      })
       .map((item) => (
         <CollectionItem key={item.id} item={item} remove={this.removeItem} />
       ));
@@ -60,6 +75,17 @@ class Collection extends React.Component {
           value={this.state.filter}
           onChange={this.handleChange}
         />
+        <label>Order by:</label>
+        <select
+          value={this.state.orderBy}
+          onChange={this.handleChange}
+          name="orderBy"
+        >
+          <option value="name asc">Name (ascending)</option>
+          <option value="name desc">Name (descending)</option>
+          <option value="rating asc">Rating (ascending)</option>
+          <option value="rating desc">Rating (descending)</option>
+        </select>
         <div className="collection">
           <CollectionForm key="form" add={this.addItem} />
           {items}
